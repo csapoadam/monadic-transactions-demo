cf956aea69746724a09c8c76f6dd30624844b123
Adam Csapo <csapo.adam@gmail.com>
2024-02-04 01:35:57 +0100

    forgot to remove this from base class

--- repository.py
+++ repository.py
@@ -4,12 +4,6 @@ from abc import ABC, abstractmethod
 
 
 class Repository(ABC):
-    def __init__(self):
-        self.ACCOUNTS = {
-            13542: [Account(13542, "John Doe", 35000)],
-            18832: [Account(18832, "Mary Day", 42000)]
-        }
-
     ## All getters and sanity-check related functions should return a Maybe[Account]
     @abstractmethod
     def get_account(self, account_no: int) -> Maybe[Account]:
